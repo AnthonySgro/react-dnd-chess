c623a8caeb8100469c7315034da1bd627f73dcac
Reuben Bradley <reuben.m.bradley@gmail.com>
2020-08-30 18:03:48 -0400

    Fix responsiveness and minor styling

--- src/components/Board/BoardStyles.js
+++ src/components/Board/BoardStyles.js
@@ -3,9 +3,10 @@ const jssStyles = {
     border: '1px solid #c0c0c0',
     display: 'flex',
     flexWrap: 'wrap',
-    height: '800px',
+    height: 'calc(100vw - 4rem)',
     margin: '1em auto 0',
-    width: '800px'
+    maxHeight: 'calc(100vh - 14rem)',
+    maxWidth: 'calc(100vh - 14rem)'
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 
 import Board from 'components/Board/';
 
+import './index.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <CssBaseline />
